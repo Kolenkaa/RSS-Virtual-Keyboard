06657205c8473a4026d344fd409f048d55e863ef
Kolenkaa <kkolenkaa@gmail.com>
2022-05-04 13:14:14 +0300

    feat: add keyboard layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -17,6 +18,7 @@ module.exports = {
 			template: 'src/index.html'
 		}),
 		new MiniCssExtractPlugin(),
+		new ESLintPlugin(),
 	],
 	module: {
 		rules: [
